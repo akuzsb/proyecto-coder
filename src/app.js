a5ef3e02c38720791f4ab08776124a26727e76c6
Augusto Santillan <90767789+akuzsb@users.noreply.github.com>
2024-01-13 21:50:03 -0300

    Add routes for products and carts

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
-
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+import productsRouter from './routes/products.router.js';
+import cartsRouter from './routes/carts.router.js';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => { res.send('Hello World!') });
+
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
 
 app.listen(8080, () => {
     console.log('App corriendo en el puerto 8080 http://localhost:8080');
