d61204084ce2206fafb07f6fdce6085e4a13cae3
Augusto Santillan <90767789+akuzsb@users.noreply.github.com>
2024-02-09 20:59:15 -0300

    Add handlebars engine, socket.io server, and MongoDB connection

--- app.js
+++ app.js
@@ -1,17 +1,67 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
+import 'dotenv/config';
+import { Server } from 'socket.io';
+import mongoose from 'mongoose';
+import __dirname, { MONGO_URI } from './src/utils.js';
 const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+const httpServer = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+export const socketServer = new Server(httpServer);
+
+app.engine('handlebars', engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+
 
 import productsRouter from './src/routes/products.router.js';
 import cartsRouter from './src/routes/carts.router.js';
+import productsViewRouter from './src/routes/productsViewRouter.js';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => { res.send('Hello World!') });
+app.get('/', (req, res) => { res.redirect('/home') });
+
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
+app.use('/products', productsViewRouter);
+app.get('/chat', (req, res) => {
+    res.render('chat');
+});
 
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-app.listen(8080, () => {
-    console.log('App corriendo en el puerto 8080 http://localhost:8080');
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Error connecting to MongoDB', err));
+
+
+import MessagesDAO from './src/dao/messagesDao.js';
+
+socketServer.on('connection', async (socket) => {
+
+    let messages = await MessagesDAO.getMessages();
+    socket.emit('messages', messages);
+
+    socket.on('new-message', async (data) => {
+        await MessagesDAO.addMessage(data);
+        messages = await MessagesDAO.getMessages();
+        socketServer.emit('messages', messages);
+    });
+
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 - Not Found');
 });
