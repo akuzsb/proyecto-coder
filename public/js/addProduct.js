ad3438c2223fd22cc316b93936e333bdf52a8a15
Augusto Santillan <90767789+akuzsb@users.noreply.github.com>
2024-02-09 20:58:25 -0300

    Add product form and upload middleware, remove CartManager and ProductManager

---
+++ public/js/addProduct.js
@@ -0,0 +1,33 @@
+const randomCode = (length) => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+const formulario = document.querySelector('#productForm');
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const product = new FormData(formulario);
+    fetch('/api/products', {
+        method: 'POST',
+        body: product,
+        
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data, 'data')
+        if (data.message) {
+            alert(data.message);
+            if (data.id){
+
+                window.location.href = `/products/${data.id}`;
+            }
+        }
+    })
+    .catch(err => console.log(err));
+
+    // formulario.reset();
+});
