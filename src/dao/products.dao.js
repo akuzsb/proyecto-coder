f7897b07f6977a63ebd9b73b80d3c197883cd257
Augusto Santillan <90767789+akuzsb@users.noreply.github.com>
2024-02-20 17:16:44 -0300

    Refactor getAll and add getAllPaginated methods in ProductsDAO

--- src/dao/products.dao.js
+++ src/dao/products.dao.js
@@ -2,15 +2,21 @@ import productsSchema from "../schemas/products.schema.js";
 
 class ProductsDAO {
 
-    static async getAll({ limit = null, orderBy = 'title-asc' }) {
-        let order = orderBy.split('-');
-        let sortBy = {};
-        sortBy[order[0]] = order[1] === 'asc' ? 1 : -1;
-        const products = await productsSchema.find().sort(sortBy).lean();
-        return {
-            products,
-            orderBy
-        }
+    static async getAllPaginated({ limit = 10, order = null, page = 1 }) {
+
+        let config = {
+            limit,
+            page,
+            lean: true,
+            sort: order ? { [order.split('-')[0]]: order.split('-')[1] === 'desc' ? -1 : 1 } : undefined
+        };
+
+        return await productsSchema.paginate({}, config);
+    }
+
+    static async getAll({ limit = 10, orderBy = null }) {
+        let products = await productsSchema.find().limit(limit).sort(orderBy ? { [orderBy.split('-')[0]]: orderBy.split('-')[1] === 'desc' ? -1 : 1 } : undefined).lean();
+        return { products, orderBy };
     }
 
     static async getById(id) {
