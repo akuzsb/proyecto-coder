b8f16d5bf30423e694a7df7fe3d05120a5108084
Augusto Santillan <90767789+akuzsb@users.noreply.github.com>
2024-02-25 22:35:22 -0300

    Refactor routes to include checkLogin middleware

--- src/routes/cartsViewRouter.js
+++ src/routes/cartsViewRouter.js
@@ -1,13 +1,14 @@
-import e, { Router } from 'express';
+import { Router } from 'express';
+import {checkLogin} from '../middlewares/checkLogin.js';
 const router = Router();
 
 import CartsDAO from '../dao/carts.dao.js'
 
-router.get('/', (req, res) => {
+router.get('/', checkLogin, (req, res) => {
     res.render('error', { message: 'Hubo un error al obtener el carrito' });
 });
 
-router.get('/:cid', async (req, res) => {
+router.get('/:cid', checkLogin, async (req, res) => {
     const { cid } = req.params;
     try {
         const [cart] = await CartsDAO.getByIdPopulate(cid);
--- src/routes/productsViewRouter.js
+++ src/routes/productsViewRouter.js
@@ -1,33 +1,33 @@
 import { Router } from 'express';
 import ProductsDAO from '../dao/products.dao.js';
-
+import { checkLogin } from '../middlewares/checkLogin.js';
 const router = Router();
 
-router.get('/', async (req, res) => {
+router.get('/', checkLogin, async (req, res) => {
     let { limit, order, page, query } = req.query;
     page = page ? parseInt(page) : 1;
     limit = limit ? parseInt(limit) : 8;
     try {
-        const products = await ProductsDAO.getAllPaginated({limit, order, page, query});
+        const products = await ProductsDAO.getAllPaginated({ limit, order, page, query });
         products.prevLink = products.hasPrevPage ? `/products?limit=${limit}&order=${order}&page=${products.prevPage}&query=${query}` : null;
         products.nextLink = products.hasNextPage ? `/products?limit=${limit}&order=${order}&page=${products.nextPage}&query=${query}` : null;
         products.isValid = (page > 0 && page <= products.totalPages);
-        res.render("products", { products, orderBy: order});
+        res.render("products", { products, orderBy: order });
     } catch (error) {
         console.log(error)
         res.render('error', { message: 'Hubo un error al obtener los productos' });
     }
 });
 
-router.get('/agregar', (req, res) => {
+router.get('/agregar', checkLogin, (req, res) => {
     res.render('addProduct');
 });
 
-router.get('/:id/editar', async (req, res) => {
+router.get('/:id/editar', checkLogin, async (req, res) => {
     let { id } = req.params;
     try {
         let [product] = await ProductsDAO.getById(id);
-        if (!product){
+        if (!product) {
             return res.render('error', { message: 'Producto no encontrado' });
         }
         res.render("editProduct", { product });
@@ -37,11 +37,11 @@ router.get('/:id/editar', async (req, res) => {
     }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', checkLogin, async (req, res) => {
     let { id } = req.params;
     try {
         let [product] = await ProductsDAO.getById(id);
-        if (!product){
+        if (!product) {
             return res.render('error', { message: 'Producto no encontrado' });
         }
         res.render("product", { product });
