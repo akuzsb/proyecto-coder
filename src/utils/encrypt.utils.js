b886671f5737c1f2a25e945b9716e0a41439b85a
Augusto Santillan <90767789+akuzsb@users.noreply.github.com>
2024-03-04 19:08:47 -0300

    Add encrypt and compare password utility functions

---
+++ src/utils/encrypt.utils.js
@@ -0,0 +1,12 @@
+import bcrypt from 'bcrypt';
+
+const encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+export { encryptPassword, comparePassword };
