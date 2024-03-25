4021fa96d09830aa14c4b6f1a4d2d01a5400935d
Augusto Santillan <90767789+akuzsb@users.noreply.github.com>
2024-03-25 10:41:21 -0300

    Add getCartByIdPopulate route handler to cartsViewRouter.js

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -120,3 +120,19 @@ export const deleteAllProductsFromCart = async (req, res) => {
         res.status(500).send({ error: 'Hubo un error al eliminar los productos del carrito' });
     }
 }
+
+export const getCartByIdPopulate = async (req, res) => {
+    const { cid } = req.params;
+    try {
+        const [cart] = await CartsDAO.getByIdPopulate(cid);
+        if (!cart) return res.render('error', { message: 'carrito no encontrado' });
+        cart.products = cart.products.map(p => {
+            p.total = (p.product.price * p.quantity).toFixed(2);
+            return p;
+        });
+        res.render('cart', { cart });
+    } catch (error) {
+        console.log(error)
+        res.render('error', { message: 'Hubo un error al obtener el carrito' });
+    }
+}
--- src/routes/cartsViewRouter.js
+++ src/routes/cartsViewRouter.js
@@ -3,25 +3,12 @@ import {checkLogin} from '../middlewares/checkLogin.js';
 const router = Router();
 
 import CartsDAO from '../dao/carts.dao.js'
+import { getCartByIdPopulate } from '../controllers/carts.controller.js';
 
 router.get('/', checkLogin, (req, res) => {
-    res.render('error', { message: 'Hubo un error al obtener el carrito' });
+    res.render('error', { message: 'Es necesario id del carrito' });
 });
 
-router.get('/:cid', checkLogin, async (req, res) => {
-    const { cid } = req.params;
-    try {
-        const [cart] = await CartsDAO.getByIdPopulate(cid);
-        if (!cart) return res.render('error', { message: 'carrito no encontrado' });
-        cart.products = cart.products.map(p => {
-            p.total = (p.product.price * p.quantity).toFixed(2);
-            return p;
-        });
-        res.render('cart', { cart });
-    } catch (error) {
-        console.log(error)
-        res.render('error', { message: 'Hubo un error al obtener el carrito' });
-    }
-});
+router.get('/:cid', checkLogin, getCartByIdPopulate);
 
 export default router;
