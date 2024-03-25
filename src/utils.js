a45ea1a56290383fed4d32c42f57ca21d7d3c739
Augusto Santillan <90767789+akuzsb@users.noreply.github.com>
2024-03-25 10:45:32 -0300

    Update MONGO_URI in utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,8 @@ import {dirname} from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export const MONGO_URI=`mongodb+srv://as:${process.env.MONGO_PASS}@cluster-as.9xjc9xq.mongodb.net/ecommerce`
+// export const MONGO_URI=`mongodb+srv://as:${process.env.MONGO_PASS}@cluster-as.9xjc9xq.mongodb.net/ecommerce`
+export const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/ecommerce';
 
 
 export default __dirname;
